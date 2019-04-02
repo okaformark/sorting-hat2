3574813dc58e5c00a91a5d43a26e79acfaf51937
Mark Okafor <okaformark@gmail.com>
2019-04-01 19:46:52 -0500

    bootstrap form clears

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
+let hogwartsHouse = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
 const startButton = document.getElementById('startButton');
-const sortButton = document.getElementById('sortButton');
-const inputText = document.getElementById('nameInput');
+//const inputText = document.getElementById('nameInput');
 //printToDom function
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
 };
+const clearForm = () => {
+    const inputText = document.getElementById('sortingId');
+     inputText.innerHTML = '';
+ };
+
+const studentCardBuilder = (cardToPrint) =>{
+    let randomHouse = Math.floor((Math.random() * hogwartsHouse.length));
+    let studentCard = '';
+    cardToPrint.forEach((student) => {
+        studentCard += `<div class="card" style="width: 18rem;">`;
+        studentCard +=      `<div class="card-body">`;
+        studentCard +=          `<h5 class="card-title">${inputText.value}</h5>`;
+        studentCard +=          `<p class="card-text">Your new House is <h2>${randomHouse[hogwartsHouse]}!!!</p>`;
+        studentCard +=          `<button href="#" class="btn btn-danger deleteButton">Expel!</button>`;
+        studentCard +=      `</div>`;
+        studentCard += `</div>`;
+        printToDom ('studentId', studentCard);
+    });
+};
+
 const showSortForm = () => {
     let domString = '';
     domString += `<form>`;
@@ -18,15 +38,34 @@ const showSortForm = () => {
     domString +=    `<button type="submit" class="btn btn-primary" id = "sortButton">Sort</button>`;
     domString += `</form>`;
 
+    
     printToDom('sortingId', domString);
-};
-
-const clearForm = () => {
-    domString = '';
-    inputText.value = '';
-};
-const buttonEvent = () => {
-    startButton.addEventListener('click', showSortForm);
+    const sortButton = document.getElementById('sortButton');
     sortButton.addEventListener('click', clearForm);
 };
+
+
+
+
+const buttonEvent = () => {
+    startButton.addEventListener('click', showSortForm);
+   
+    // if (sortButton) {
+    // addEventListener('click', function (e) {
+    //     clearForm();
+    // });
+    // };
+};
 buttonEvent();
+
+
+
+
+
+
+
+
+
+
+
+
